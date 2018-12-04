cc43ce0a9e2b5329f1152fe0afd204017d3fe1dc
Vladimir Smiljanic <vladsmiljanic@gmail.com>
2018-12-03 20:57:20 -0500

    Adding Mail Api

--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,8 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const cors = require('cors')({origin:'http://localhost:3000'});
 
-const API = "489bfc00741d0809d395140cd05793d6-9525e19d-d875dfc7";
-const DOMAIN = "www.omniumcpg.com";
+const API = process.env.MAIL_API;
+const DOMAIN = process.env.DOMAIN;
 
 const mailgun = require('mailgun-js')({apiKey:API, domain:DOMAIN, testMode:true});
 
